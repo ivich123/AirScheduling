f60807eeddb6fc6e61979e767e0a3f94f82369a4
Marc Garcia <marc.garcia.roig@gmail.com>
2014-12-28 18:41:01 +0100

    coses d'estil

--- prac.cpp
+++ prac.cpp
@@ -16,48 +16,34 @@ struct trayecto{
 
 vector<trayecto> trayectos;
 
-typedef vector <vector <int> > Matrix;
+typedef vector <vector <int> > Grafo;
 
-Matrix g;
+Grafo g;
 
 
 void buildGraph(){
-  for (int i = 0; i < trayectos.size(); ++i)
-  {
-      ++g[trayectos[i].IDor][trayectos[i].IDdest];
-      //nodo S
-      g[g.size()-2][trayectos[i].IDor] = 1;
-      //nodo T
-      g[trayectos[i].IDdest][g.size()-1] = 1;
-      //nodo s'
-      g[g.size()-3][trayectos[i].IDdest] = 1;
-      //nodo t'
-      g[trayectos[i].IDor][g.size()-4] = 1;
-  } 
-      g[g.size()-2][g.size()-4] = 1;
-      g[g.size()-3][g.size()-1] = 1;
+    for (int i = 0; i < trayectos.size(); ++i) {
+        ++g[trayectos[i].IDor][trayectos[i].IDdest];
+        //nodo S
+        g[g.size()-2][trayectos[i].IDor] = 1;
+        //nodo T
+        g[trayectos[i].IDdest][g.size()-1] = 1;
+    }
 
-  for (int i = 0; i < trayectos.size(); ++i)
-  {
-      for (int j = 0; j < trayectos.size(); ++j)
-      {
-            if(i != j and trayectos[j].hsal - trayectos[i].hlleg >= 15){
+    for (int i = 0; i < trayectos.size(); ++i) {
+        for (int j = 0; j < trayectos.size(); ++j) {
+            if(i != j and trayectos[j].hsal - trayectos[i].hlleg >= 15) {
                 //cout<<trayectos[i].hlleg<< " " << trayectos[j].hsal << " " << endl;
                 //cout << "desti = " << trayectos[i].IDdest << "  orig = " << trayectos[j].IDor << endl;
                 ++g[trayectos[i].IDdest][trayectos[j].IDor];
             }
-      }
-  }
-    for (int i = 0; i < trayectos.size(); ++i) {
-        g[trayectos[i].IDor][trayectos[i].IDdest] = 0;
+        }
     }
 }
 
 void escriu(){
-    for (int i = 0; i < g.size(); ++i)
-    {
-        for (int j = 0; j < g.size(); ++j)
-        {
+    for (int i = 0; i < g.size(); ++i) {
+        for (int j = 0; j < g.size(); ++j) {
             cout << g[i][j] << " " ;
         }
         cout << endl;
@@ -65,7 +51,7 @@ void escriu(){
 }
 
 int edmonsKarp(){
-    
+
 }
 
 int main(){
@@ -89,7 +75,7 @@ int main(){
         trayectos.push_back(t);
         ++max;
     }
-    g = Matrix(max*2+4,vector<int>(max*2+4,0));
+    g = Grafo(max*2+2,vector<int>(max*2+2,0));
     buildGraph();
     int maxFlow = edmonsKarp();
     escriu();
